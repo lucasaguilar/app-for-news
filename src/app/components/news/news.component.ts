a46c8c83634b5ae44c18fedba3eb5e5475e80f94
Lucas Aguilar <lucasaguilar@gmail.com>
2022-01-25 11:36:06 -0300

    add actionSheets for shared, add to favorites or cancel the news.

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/models';
 import { Browser } from '@capacitor/browser';
-import { Platform } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-news',
@@ -12,7 +12,10 @@ export class NewsComponent implements OnInit {
   @Input() article: Article = null;
   @Input() indice = 0;
 
-  constructor(private platform: Platform) {}
+  constructor(
+    private platform: Platform,
+    private actionSheetController: ActionSheetController
+  ) {}
 
   ngOnInit() {}
 
@@ -23,5 +26,48 @@ export class NewsComponent implements OnInit {
       // this line only work in web browsers
       window.open(this.article.url, '_blank');
     }
+  }
+
+  async onOpenMenu() {
+    const action = await this.actionSheetController.create({
+      header: 'Options',
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          handler: () => {
+            this.sharedArticle();
+          },
+        },
+        {
+          text: 'Add to favorites',
+          icon: 'heart-outline',
+          handler: () => {
+            this.addToFavorites();
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'exit-outline',
+          handler: () => {
+            this.cancel();
+          },
+        },
+      ],
+    });
+
+    await action.present();
+  }
+
+  sharedArticle() {
+    console.log('article to by shared...');
+  }
+
+  addToFavorites() {
+    console.log('addToFavorites...');
+  }
+
+  cancel() {
+    console.log('cancel...');
   }
 }
